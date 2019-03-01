07c5982b4f3b650ecb12cd1a6f6a28fb1e59654e
schinvendy <schinvendy@icloud.com>
2019-03-01 17:17:35 +0800

    update v1.2.0

--- src/components/countdown/index.js
+++ src/components/countdown/index.js
@@ -12,58 +12,65 @@ export default class Countdown extends Component {
     targetTime: '', // 目标时间
     showText: false, //  显示时分秒
     showDay: false, // 显示天数
-    color: '#111', // 字体颜色
+    color: 'inherit', // 字体颜色
     symbol: ':', // 间隔符号
     isClose: false, // 手动关闭倒计时
     onTick: () => {}, // 倒计时过程事件
     onEnd: () => {}, //倒计时结束事件
   };
 
-  state = {
-    day: '00',
-    hour: '00',
-    minute: '00',
-    second: '00',
-    targetTimestamp: '',
-  };
+  constructor() {
+    super(...arguments);
+    this.state = {
+      day: '0',
+      hour: '00',
+      minute: '00',
+      second: '00',
+    };
+    this.targetTimestamp = null;
+  }
 
   componentDidMount() {
     this.formatTargetTime(this.props.targetTime);
   }
 
   componentWillUnmount() {
-    this.setState({ targetTimestamp: '' });
+    this.targetTimestamp = null;
   }
 
-  componentWillReceiveProps(props) {
-    const { targetTime } = props;
-    const { targetTimestamp } = this.state;
-    !targetTimestamp && this.formatTargetTime(targetTime);
+  componentDidHide() {
+    this.targetTimestamp = null;
+  }
+
+  componentDidShow() {
+    this.formatTargetTime(this.props.targetTime);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { targetTime } = nextProps;
+    if (targetTime !== this.props.targetTime) {
+      this.formatTargetTime(targetTime);
+    }
   }
 
   // 处理日期格式
-  formatTargetTime(targetTime = '') {
+  formatTargetTime(targetTime) {
+    if (!targetTime) {
+      return;
+    }
     // 避免iOS端上的日期格式有问题
     let time = targetTime.replace(/-/g, '/');
-    this.setState(
-      {
-        targetTimestamp: new Date(time).getTime() || '',
-      },
-      () => {
-        this.getRemainingSecond();
-      },
-    );
+    this.targetTimestamp = new Date(time).getTime();
+    this.getRemainingSecond();
   }
 
   // 计算剩余时间秒数
   getRemainingSecond() {
-    if (!this.state.targetTimestamp) return;
-    // 目标时间
-    let targetTimestamp = this.state.targetTimestamp;
+    if (!this.targetTimestamp) return;
     // 当前时间
     let currentTimestamp = new Date().getTime();
     // 剩余时间
-    let remainingSecond = Math.floor((targetTimestamp - currentTimestamp) / 1000);
+    let remainingSecond = Math.floor((this.targetTimestamp - currentTimestamp) / 1000);
     // 天 时 分 秒
     let day = '';
     let hour = '';
@@ -78,7 +85,8 @@ export default class Countdown extends Component {
       second = this.formatNum(parseInt((remainingSecond % 3600) % 60));
       this.timeTick(remainingSecond);
     } else {
-      day = hour = minute = second = '00';
+      day = '0';
+      hour = minute = second = '00';
       this.onTimeEnd();
     }
     this.setTimeState(day, hour, minute, second);
@@ -87,7 +95,6 @@ export default class Countdown extends Component {
   // 设置时间
   setTimeState(day, hour, minute, second) {
     // 是否显示天时分秒文字
-    this.props.showDay && (day += '天');
     if (this.props.showText) {
       hour += '时';
       minute += '分';
@@ -116,24 +123,31 @@ export default class Countdown extends Component {
 
   // 倒计时结束触发事件
   onTimeEnd() {
+    this.targetTimestamp = null;
     this.props.onEnd && this.props.onEnd();
   }
 
   render() {
-    const { withBorder, color, symbol, showDay, className } = this.props;
+    const { color, symbol, showDay, className } = this.props;
     const { day, hour, minute, second } = this.state;
     // 末尾有空格
     let countdownClass = 'countdown ';
-    if (withBorder) countdownClass += 'with-border ';
     if (className) countdownClass += className;
     const countdownStyle = {
       color: color,
     };
     return (
       <View className={countdownClass} style={countdownStyle}>
-        {showDay && <Text className="day">{day}</Text>}
-        <Text className="hour">{hour}</Text>
-        <Text className="symbol">{symbol}</Text>
+        {showDay && day !== '00' && (
+          <Text>
+            <Text className="day">{day}</Text>
+            <Text className="day-text">天</Text>
+          </Text>
+        )}
+        <Text>
+          <Text className="hour">{hour}</Text>
+          <Text className="symbol">{symbol}</Text>
+        </Text>
         <Text className="minute">{minute}</Text>
         <Text className="symbol">{symbol}</Text>
         <Text className="second">{second}</Text>
